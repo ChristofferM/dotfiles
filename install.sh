a3367b7eb010e94798ada045a56ae3d15a7d9b82
Christoffer Malmgren <christoffer.malmgren@gmail.com>
2021-11-14 17:29:56 +0100

    Use system default in bashrc

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-here=~/Dev/dotfiles/configs/
-ln -svf ${here}.bashrc ~/.bashrc
+script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+here="${script_dir}/configs/"
+
+# System default ~/.bashrc might hava a nice settings. Source your settings in the end
+if grep -q 'BASH SOURCED FROM' ~/.bashrc; then
+  echo "Bash already sourced"
+else
+  echo "# DO NOT EDIT. BASH SOURCED FROM ${here} at end of file" | cat - ~/.bashrc > temp && mv temp ~/.bashrc
+  echo 'source ~/Dev/dotfiles/configs/.bashrc' >> ~/.bashrc
+  echo "Appending source of your bashrc"
+fi
+
 ln -svf ${here}.tmux.conf ~/.tmux.conf
 ln -svf ${here}.gitconfig ~/.gitconfig
 ln -svf ${here}init.vim ~/.config/nvim/init.vim
